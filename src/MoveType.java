403d214a9443501cc016a13fd3b3b7d6429ac14a
Burhan27 <s175446@student.dtu.dk>
2019-11-25 15:04:00 +0100

    movetype documentatios

--- src/MoveType.java
+++ src/MoveType.java
@@ -1,12 +1,12 @@
 public enum MoveType {
 
-    Kill,
-    Standard,
-    Illegal,
-    KingStandard,
-    KingIllegal,
-    CrownKing,
-    CrownKingKill,
-    CrownKingSlay,
-    KingSlay,
+    Kill, //standard kill - killed a non-king piece.
+    Standard, //non-king piece move to an empty diagonal.
+    Illegal, //invalid moves.
+    KingStandard, //basic move of king to empty diagonal
+    KingIllegal, //invalid king move
+    CrownKing, //move where a non-king piece becomes king (reaching the other side of the board)
+    CrownKingKill, //a non-king piece kills another non-king piece and reaches the other side of the board.
+    CrownKingSlay, //a non-king piece kills another king piece and reaches the other side of the board.
+    KingSlay, //a piece kills a king piece
 }
